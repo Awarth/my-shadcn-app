fcf800f45cadc4a2adb08d2ae5089e375a6ef34d
aguyran <aguyran@gmail.com>
2025-06-11 02:31:28 +0530

    fix: build scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
-import WalshRoute from "@/pages/routes";
+import { BrowserRouter } from 'react-router-dom';
+import WalshRoute from '@/pages/routes';
 
 function App() {
   return (
-    <BrowserRouter basename="/walsh">
+    <BrowserRouter basename="/parent-app">
       <WalshRoute />
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from 'vite'
+import path from 'path';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import proxyOptions from './proxyOptions'
+import proxyOptions from './proxyOptions';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    port: 8080,
-    proxy: proxyOptions,
-  },
   plugins: [react()],
+  server: {
+    port: 8080,
+    proxy: proxyOptions
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': path.resolve(__dirname, 'src')
+    }
   },
-})
+  build: {
+    outDir: '../unity_parent_app/public/new_frontend',
+    emptyOutDir: true,
+    target: 'es2015',
+  },
+});
